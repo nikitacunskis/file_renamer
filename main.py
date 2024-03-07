7d62a8117fbcf178dfdd506d645e9c9a974613ec
Nikita Cunskis <85262150+nikitacunskis@users.noreply.github.com>
2024-03-07 19:03:26 +0200

    Numbers are now customizable

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ class ConfigReader:
         self.config_file = config_file
 
     def get_config(self):
+        print("Reading configuration...")
         try:
             with open(self.config_file, 'r') as file:
                 config = json.load(file)
+                print("Configuration successfully read.")
                 return config
         except FileNotFoundError:
             msg = "Configuration file not found."
@@ -48,10 +50,12 @@ class DirectoryReader:
         logging.info(msg)
 
     def list_files(self):
+        print("Listing files in directory...")
         files_info = []
         try:
             for filename in os.listdir(self.folder_path):
                 if filename.endswith('.mp4'):
+                    print(f"Processing file: {filename}")
                     path = os.path.join(self.folder_path, filename)
                     if os.path.isfile(path):
                         number_match = re.search(r'_([0-9]+)\.mp4$', filename)
@@ -69,12 +73,17 @@ class DirectoryReader:
             msg = f"An error occurred while listing files: {e}"
             print(msg)
             logging.error(msg)
+        print("Finished listing files.")
         return files_info
 
     def read_video_length(self, file_path):
+        print(f"Reading video length for: {file_path}")
         try:
             with VideoFileClip(file_path) as video:
-                return round(video.duration)
+                duration = round(video.duration)
+                print(f"Video length: {duration} seconds.")
+                return duration
+
         except Exception as e:
             msg = f"Error reading video length: {e}"
             print(msg)
@@ -82,47 +91,52 @@ class DirectoryReader:
             return 0
 
     def generate_new_filenames(self):
+        print("Generating new filenames...")
         files_info = self.list_files()
+        files_info.sort(key=lambda x: int(x['number']))
         new_filenames_info = []
         numbers_seen = {}
         duplicates = set()
         all_numbers = []
 
         for file_info in files_info:
-            # Convert to int for sorting and comparison
             number = int(file_info['number'])
             all_numbers.append(number)
             if number in numbers_seen:
                 duplicates.add(number)
                 msg = f"Warning: Duplicate number detected for {number}. Ignoring files: {numbers_seen[number]}, {file_info['filename']}"
-                print(f"\033[91m{msg}\033[0m")
+                print(msg)
                 logging.warning(msg)
             else:
                 numbers_seen[number] = file_info['filename']
 
-        # Sort and remove duplicates
         all_numbers = sorted(list(set(all_numbers)))
-        missing_numbers = [x for x in range(
-            all_numbers[0], all_numbers[-1] + 1) if x not in all_numbers]
+        if all_numbers and all_numbers[0] == 0:
+            user_input = input(
+                "The first number is 0. Do you want to adjust all numbers by incrementing them by 1? (yes/no): ")
+            if user_input.strip().lower() == 'yes':
+                all_numbers = [x + 1 for x in all_numbers]
+                numbers_seen = {k + 1: v for k, v in numbers_seen.items()}
+        missing_numbers = [x for x in range(all_numbers[0], all_numbers[-1] + 1) if x not in all_numbers]
 
         if missing_numbers:
             missing_numbers_str = ", ".join(map(str, missing_numbers))
-            msg = f"\033[91mMissing numbers detected: {missing_numbers_str}.\033[0m"
+            msg = f"Missing numbers detected: {missing_numbers_str}."
             print(msg)
             logging.info(msg)
 
-        incrementer = 1
         for file_info in files_info:
-            if file_info['number'] not in duplicates:  # Ignore duplicates
+            if file_info['number'] not in duplicates:
+                adjusted_number = int(file_info['number']) + 1 if all_numbers[0] == 1 else int(file_info['number'])
                 new_filename = self.filename_template.format(
                     date=file_info['date'],
                     size_marker=file_info['size_marker'],
-                    number=str(incrementer).zfill(6),
+                    number=str(adjusted_number).zfill(6),
                     length_seconds=str(file_info['length_seconds']).zfill(3)
                 ) + ".mp4"
                 new_filenames_info.append(
                     (file_info, file_info['filename'], new_filename))
-                incrementer = incrementer + 1
+        print("New filenames generated.")
 
         return new_filenames_info
 
@@ -130,15 +144,14 @@ class DirectoryReader:
         new_filenames = self.generate_new_filenames()
 
         print("Proposed file renames (excluding duplicates):")
-        logging.info("Proposed file renames (excluding duplicates):")
         for _, old_filename, new_filename in new_filenames:
             msg = f"{old_filename} -> {new_filename}"
             print(msg)
-            logging.info(msg)
 
         user_response = input("Do you want to rename the files? (yes/no): ")
         if user_response.lower() in ['yes', 'y']:
             for _, old_filename, new_filename in new_filenames:
+                print(f"Renaming {old_filename} to {new_filename}...")
                 old_path = os.path.join(self.folder_path, old_filename)
                 new_path = os.path.join(self.folder_path, new_filename)
                 try:
@@ -151,10 +164,8 @@ class DirectoryReader:
                     print(msg)
                     logging.error(msg)
             print("File renaming completed.")
-            logging.info("File renaming completed.")
         else:
             print("File renaming cancelled.")
-            logging.info("File renaming cancelled.")
 
 
 # Main execution
